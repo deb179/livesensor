9fba5fcd432796a24f941e67aa7221421aa91975
deb179 <debi123patra@gmail.com>
2025-12-27 13:15:27 +0530

    logger and exception

--- setup.py
+++ setup.py
@@ -1,19 +1,50 @@
-from setuptools import find_packages, setup
+# from setuptools import find_packages ,setup 
+# from typing import List
+
+
+
+# def get_requirements()->List[str]:
+
+#     reuirements_list : List[str] =[]
+
+#     return reuirements_list
+
+
+
+
+
+
+# setup (
+#     name = 'sensor',
+#     version="0.0.1",
+#     author="prince",
+#     author_email="princekatiyar986@gmail.com",
+#     packages = find_packages(),
+#     install_requires = get_requirements() , #["pymongo"]
+
+# )
+
+from setuptools import find_packages,setup
 from typing import List
 
-def get_requirements()->list[str]:
-    requirements_list : list[str]=[]
+def get_requirements()->List[str]:
+    """
 
-    return requirements_list
 
+    This function will return list of requirements
+    """
+    requirement_list:List[str] = []
+
+    """
+    Write a code to read requirements.txt file and append each requirements in requirement_list variable.
+    """
+    return requirement_list
 
 setup(
-name="sensor",
-version="0.0.1",
-author="debiprasad",
-author_email="debi123patra@gmail.com",
-packages= find_packages(),
-install_requires = get_requirements() #["pymongo"]
-
-
+    name="sensor",
+    version="0.0.1",
+    author="ineuron",
+    author_email="princekatiyar986@gmail.com",
+    packages = find_packages(),
+    install_requires=get_requirements(),#["pymongo==4.2.0"],
 )
